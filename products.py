14872c4d1e2e81a7b7c6e690fb84487389b5513a
Danny Lin <d033016@gmail.com>
2019-08-03 15:05:36 +0800

    add check function

--- products.py
+++ products.py
@@ -1,11 +1,16 @@
+import os
 products = []
-with open('products.csv', 'r', encoding = 'utf-8')as f:
-    for line in f:
-        if '商品名稱,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+    print('找到檔案')
+    with open('products.csv', 'r', encoding = 'utf-8')as f:
+        for line in f:
+            if '商品名稱,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('未找到檔案')
 while True:
     name = input("請輸入商品名稱:")
     if name == "q":
